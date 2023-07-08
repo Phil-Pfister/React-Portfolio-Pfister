6df3ba0d628d74ba63d73913df4d19e5c3c821c1
Phil-Pfister <salshouse@gmail.com>
2023-07-08 09:59:08 -0500

    created file structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+function Header() {
+    return <>
+        <div className="container">
+  <nav className="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark">
+    <div className="container-fluid">
+    <h1 className="text-light">Phillip Pfister</h1>
+      <a className="navbar-brand text-light" href="#">Portfolio</a>
+    </div>
+  </nav>
+</div>
+    </>
+}
+
+export default Header;
