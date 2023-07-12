871f3a433f67a8feb0a99623d479fde76da597aa
Phil-Pfister <salshouse@gmail.com>
2023-07-12 13:02:13 -0500

    image bug fix

--- src/components/pages/works.js
+++ src/components/pages/works.js
@@ -2,15 +2,15 @@ const works = [
     {
         id: 1,
         name: "Houston Hotspots",
-        image: "./src/assets/images/hotspots.png",
+        image: "./images/hotspots.png",
         alt: "Houston Hotspots App",
-        gitLink: "https://github.com/jomaduagwu/project-houston-hot-spots",
+        gitLink: "https://github.com/Phil-Pfister/project-houston-hot-spots",
         url: "https://jomaduagwu.github.io/project-houston-hot-spots/",
     },
     {
         id: 2,
         name: "Meal Planner",
-        image: "./src/assets/images/meal-planner.png",
+        image: "./images/meal-planner.png",
         alt: "Meal Planner app",
         gitLink: "https://github.com/Phil-Pfister/HWP_Meal-Planner",
         url: "https://young-gorge-82099.herokuapp.com/",
@@ -18,7 +18,7 @@ const works = [
     {
         id: 3,
         name: "Tech Blog",
-        image: "./src/assets/images/tech-blog.png",
+        image: "./images/tech-blog.png",
         alt: "Tech Blog",
         gitLink: "https://github.com/Phil-Pfister/Phils-Tech-Blog",
         url: "https://phil-tech-blog.herokuapp.com/ ",
@@ -26,7 +26,7 @@ const works = [
     {
         id: 4,
         name: "Weather Dashboard",
-        image: "./src/assets/images/weather.png",
+        image: "./images/weather.png",
         alt: "Weather Dashboard App",
         gitLink: "https://github.com/Phil-Pfister/Weather-Dashboard",
         url: "https://phil-pfister.github.io/Weather-Dashboard",
@@ -34,7 +34,7 @@ const works = [
     {
         id: 5,
         name: "Daily Scheduler",
-        image: "./src/assets/images/daily.jpeg",
+        image: "./images/daily.jpeg",
         alt: "Daily Scheduler App",
         gitLink: "https://github.com/Phil-Pfister/daily_schedule",
         url: " https://phil-pfister.github.io/daily_schedule/",
@@ -42,7 +42,7 @@ const works = [
     {
         id: 6,
         name: "Text Editor",
-        image: "./src/assets/images/jate.png",
+        image: "./images/jate.png",
         alt: "Text Editor App",
         gitLink: "https://github.com/Phil-Pfister/PWA-text-editor",
         url: "https://historic-glacier-bay-99935-8b5ec286ac12.herokuapp.com/",
