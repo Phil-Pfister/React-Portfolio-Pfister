b7316ed1055d6571fc3655df3f98cf66dc93422b
Phil-Pfister <salshouse@gmail.com>
2023-07-16 10:34:19 -0500

    added content to about me

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
     {/* about section */}
     <div>
       <h2>About Me</h2>
-      <h4>
+      <h4 className="lh-lg">
         Welcome to my website! <br/>I started my career in television production in the 90s 
         and have worked for various television stations for 20 years.
         Over that time tv production has gotten more digital and automated 
@@ -15,7 +15,8 @@ export default function AboutMe() {
         I took a course at Rice University on full stack web developement and 
         discovered I have a passion for coding. <br/><br/>
         I live in Houston, Texas with my family and our husky mix, Stella.
-        I like to hike, fish, play disk golf and solve crossword puzzles. 
+        I like to hike, fish, play disk golf and solve crossword puzzles. I have a strong work ethic
+        and enjoy being part of a dedicated team that is focused on getting results.
        
       </h4>
       </div>
--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -9,7 +9,7 @@ export default function Project() {
     return <div className="row d-flex justify-content-around port-con">
             <div className="text-center pt-3">
                 <h2>Projects</h2>
-                <h4>Click on an image to visit the deployed site or the github icon to see the code</h4>
+                <h4>Click on an image to visit the deployed site or the GitHub icon to see the repo</h4>
             </div>
             {works.map((work) => <Portfolio 
                 key={work.id}
